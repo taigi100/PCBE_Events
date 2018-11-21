b027c9e6a26e80042b2944385343221089aa4caf
Robert Cazacu <taigi100@gmail.com>
2018-11-21 13:52:56 +0200

    Add default NewsBus implementation

---
+++ src/com/bigbang/NewsBus.java
@@ -0,0 +1,19 @@
+package com.bigbang;
+
+public class NewsBus implements PublisherInterface {
+
+    @Override
+    public void CreateNews(News x) {
+
+    }
+
+    @Override
+    public void UpdateNews() {
+
+    }
+
+    @Override
+    public void DeleteNews() {
+
+    }
+}
