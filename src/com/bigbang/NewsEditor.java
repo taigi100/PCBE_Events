3d4d50033230263055df1f6e330f369ee4f25d51
Robert Cazacu <taigi100@gmail.com>
2018-11-28 15:54:05 +0200

    Update NewsEditor to dispatch an event instead of calling a function

--- src/com/bigbang/NewsEditor.java
+++ src/com/bigbang/NewsEditor.java
@@ -7,7 +7,7 @@ public class NewsEditor { // need a thread for pushing events in the bus
         this.sendMessagesHandler = new Thread(() -> {
             while (true) {
                 try {
-                    nb.CreateNews(new News("test", "John Doe"));
+                    nb.AddEvent(new Event(NewsEvent.NEWS_CREATED, new News("test", "John Doe")));
                     Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
