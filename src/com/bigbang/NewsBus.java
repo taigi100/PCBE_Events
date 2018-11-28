1cbf5ba7ac3b4618afcd2da1e4e9e2137cf7911e
Robert Cazacu <taigi100@gmail.com>
2018-11-28 15:53:44 +0200

    Update NewsBus to handle events accordingly

--- src/com/bigbang/NewsBus.java
+++ src/com/bigbang/NewsBus.java
@@ -1,14 +1,12 @@
 package com.bigbang;
 
-import javafx.util.Pair;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class NewsBus implements PublisherInterface {
 
-     List<Pair<NewsEvent, News>> events;
+     List<Event> events;
      ArrayList<ConsumerCallbackInterface> subscribers;
 
     public NewsBus() {
@@ -16,28 +14,34 @@ public class NewsBus implements PublisherInterface {
         this.subscribers = new ArrayList<>();
     }
 
-    @Override
-    public void CreateNews(News x) {
-        this.events.add(new Pair<>(NewsEvent.NEWS_CREATED, x)); //TODO: should we transform this pair in an actual object?
+    private void CreateNews(News x) {
+        this.events.add(new Event(NewsEvent.NEWS_CREATED, x)); //TODO: should we transform this pair in an actual object?
         this.emitEvent(this.events.get(this.events.size() - 1));
     }
 
-    @Override
-    public void UpdateNews() { //TODO: decide how you wanna do this, make it a list with aggregated events or literally
+    private void UpdateNews() { //TODO: decide how you wanna do this, make it a list with aggregated events or literally
         // edit the element in the list
 
     }
 
-    @Override
-    public void DeleteNews() {
 
+    private void DeleteNews() {
+
+    }
+
+    @Override
+    public void AddEvent(Event event) { //TODO: Should we change this to Event and Data?
+        switch (event.getKey()){
+            case NEWS_CREATED:
+                this.CreateNews(event.getValue());
+        }
     }
 
     public void subscribe(ConsumerCallbackInterface subscriber) {
         this.subscribers.add(subscriber);
     }
 
-    private void emitEvent(Pair<NewsEvent, News> event) {
+    private void emitEvent(Event event) {
         for(ConsumerCallbackInterface subscriber : this.subscribers) {
             subscriber.cb(event.getKey(), event.getValue());
         }
